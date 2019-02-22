d43a4e812135fb8de236e4906051d6f2e2194b09
dpalay <dpalay@rapodcast.net>
2019-02-21 22:32:28 -0600

    Pager#constructor to interperate role and channel

--- PagerChannel.js
+++ PagerChannel.js
@@ -6,11 +6,11 @@ class PagerChannel {
      * @param {string} roleID associated role
      * @param {string} message Message to display.  use $role$ to represent role id and $channel$ to represent channel id
      */
-    constructor({ channelID = "", roleID = "", message = `New message! <@&$role$>` }) {
+    constructor({ channelID = "", roleID = "", message = `New message! $role$` }) {
 
         this.channelID = channelID;
         this.roleID = roleID;
-        this.message = message.replace("$role$", roleID).replace("$channel$", channelID);
+        this.message = message.replace("$role$", `<@&${roleID}>`).replace("$channel$", `<#${channelID}>`);
         this._lastmessage;
     }
 
